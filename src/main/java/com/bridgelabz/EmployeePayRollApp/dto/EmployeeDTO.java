be77783bde03789725bb0d0597b8d8b99942f88d
Abhishek <achandra961@gmail.com>
2023-04-14 22:39:46 +0530

    Validation

--- src/main/java/com/bridgelabz/EmployeePayRollApp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/EmployeePayRollApp/dto/EmployeeDTO.java
@@ -1,9 +1,16 @@
 package com.bridgelabz.EmployeePayRollApp.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.ToString;
+import lombok.Value;
+import org.intellij.lang.annotations.Pattern;
 
 @ToString
 public class EmployeeDTO {
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z]{1}[A-Za-z]{2,}$", message = "Start Name With Capital Letter")
     public String name;
+    @Min(value=500, message="Salary should be greater than 500")
     public double salary;
 }
