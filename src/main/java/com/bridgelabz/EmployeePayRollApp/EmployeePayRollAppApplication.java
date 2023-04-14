40eb7b7495e626c82c70e250c6343f6e35aeb8d0
Abhishek <achandra961@gmail.com>
2023-04-14 23:17:54 +0530

    DataBase Setting

--- src/main/java/com/bridgelabz/EmployeePayRollApp/EmployeePayRollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayRollApp/EmployeePayRollAppApplication.java
@@ -1,13 +1,20 @@
 package com.bridgelabz.EmployeePayRollApp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayRollAppApplication {
 
 	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(EmployeePayRollAppApplication.class, args);
+//		SpringApplication.run(EmployeePayRollAppApplication.class, args);
+		log.info("Application is started in {} environment", context.getEnvironment().getProperty("environment"));
+		log.info("MySQL Database UserName is ::  {}", context.getEnvironment().getProperty("spring.datasource.username"));
+		log.info("MySQL Database Password is ::  {}", context.getEnvironment().getProperty("spring.datasource.password"));
 
-		SpringApplication.run(EmployeePayRollAppApplication.class, args);
 	}
 }
