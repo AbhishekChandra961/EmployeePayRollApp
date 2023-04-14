bfb8c2f8d0d69b23db823e2e6c84fd22e797f4df
Abhishek <achandra961@gmail.com>
2023-04-14 23:10:31 +0530

    Throw Error

---
+++ src/main/java/com/bridgelabz/EmployeePayRollApp/exception/EmployeePayrollCustomException.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.EmployeePayRollApp.exception;
+
+public class EmployeePayrollCustomException extends RuntimeException{
+    public EmployeePayrollCustomException(String message){
+        super(message);
+    }
+}
